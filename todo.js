9ca8d8bfee76325cd7f9498a157fc7f7523415c6
Paul Haygood <paulthaygood@Pauls-MacBook-Air.local>
2017-07-12 14:51:22 -0400

    init

---
+++ todo.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const mustache = require('mustache');
+const bodyParser = require('body-parser');
+
+var application = express();
+application.engine('mustache', express());
+application.use(express.static(__dirname + '/public'));
+
+application.set('views', './views');
+application.set('view engine', 'mustache');
+
+application.use(bodyParser.urlencoded());
+
+var todos = [
+
+];
+
+var complete = [
+
+];
+
+application.post("/:completeId", (request, response) =>{
+    var item = request.params.completeId;
+    for( var i = 0; i< todos.length; i++) {
+        if (todos[i] === item) {
+            todos.splice(i, 1);
+            complete.push(item);
+        }
+    }
+    response.redirect('/')
+
+});
+
+application.get('/',(request, response) =>{
+    response.render('index', { todos: todos, complete: complete });
+});
+
+application.post("/", (request, response) => {
+    todos.push(request.body.todo)
+    response.redirect('/');
+
+});
+
+application.listen(3000);
